bfd537532faf8e464b2213c20ba8f9b525fe2470
RosannaRossington <rosanna_rossington@outlook.com>
2016-05-30 10:52:22 +0100

    npm install for backend and jshint resolved

--- public/js/app.js
+++ public/js/app.js
@@ -48686,23 +48686,23 @@ function Router($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "/public/src/js/views/home.html"
+      templateUrl: "/js/views/home.html"
     })
     .state('login', {
       url: "/login",
-      templateUrl: "/public/src/js/views/authentications/login.html"
+      templateUrl: "/src/js/views/authentications/login.html"
     })
     .state('register', {
       url: "/register",
-      templateUrl: "/public/src/js/views/authentications/register.html"
+      templateUrl: "/src/js/views/authentications/register.html"
     })
     .state('users', {
       url: "/users",
-      templateUrl: "/public/src/js/views/users/index.html"
+      templateUrl: "/src/js/views/users/index.html"
     })
     .state('user', {
       url: "/users/:id",
-      templateUrl: "/public/src/js/views/users/show.html",
+      templateUrl: "/src/js/views/users/show.html",
       controller: function($scope, $stateParams, User) {
         User.get({ id: $stateParams.id }, function(res){
           $scope.$parent.users.user = res.user;
